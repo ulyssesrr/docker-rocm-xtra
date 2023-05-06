46b4d8c35abaa77b5fe0f454708ef8e0eb3e9f24
Ulysses Rangel Ribeiro <ulyssesrr@gmail.com>
2023-05-05 21:52:43 -0300

    feat: rocm-gfx803-pytorch publish script

--- rocm-gfx803-pytorch/docker-build.sh
+++ rocm-gfx803-pytorch/docker-build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-source ../build-args.sh
-
-docker_image_name="ulyssesrr/rocm-gfx803-pytorch"
-docker_image_tag="rocm${ARG_ROCM_VERSION}_ubuntu${ARG_UBUNTU_VERSION}_py${ARG_PYTHON_VERSION}_pytorch${ARG_PYTORCH_VERSION}"
+source .env.build.sh
 
 docker_build -t "$docker_image_name:$docker_image_tag" .
