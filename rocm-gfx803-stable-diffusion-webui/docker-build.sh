3013d8bc99f5d939654c42fb1a67515530c26a9d
Ulysses Rangel Ribeiro <ulyssesrr@gmail.com>
2023-06-02 01:33:47 -0300

    refactor: sd-webui cleanup

--- rocm-gfx803-stable-diffusion-webui/docker-build.sh
+++ rocm-gfx803-stable-diffusion-webui/docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source .env.build.sh
+source env.build.sh
 
 docker_build --build-arg "stable_diffusion_commit=$arg_stable_diffusion_commit" -t "$docker_image_name:$docker_image_tag" .
--- rocm-gfx803-stable-diffusion-webui/docker-run.sh
+++ rocm-gfx803-stable-diffusion-webui/docker-run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-source ../build-args.sh
-arg_stable_diffusion_commit=22bcc7be428c94e9408f589966c2040187245d81
-SD_SHORT_HASH=${arg_stable_diffusion_commit:0:7}
-docker_image_name="ulyssesrr/rocm-gfx803-stable-diffusion-webui"
-docker_image_tag="${SD_SHORT_HASH}_rocm${ARG_ROCM_VERSION}_ubuntu${ARG_UBUNTU_VERSION}_py${ARG_PYTHON_VERSION}_pytorch${ARG_PYTORCH_VERSION}"
+source env.build.sh
 
 drun() {
    sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v $(pwd):/current "$@"
